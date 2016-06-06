b665b37e5d4dee4438c46c3b9168a854a187ea4b
DavidSporer <david.sporer@gmail.com>
2016-06-06 09:36:11 +0200

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,7 @@
 <?php
 // Register handler calls for Scheduler
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\Cobweb\ExternalImport\Task\AutomatedSyncTask::class] = array(
-	'extension'			=> $_EXTKEY,
+	'extension'			=> 'Coweb'.$_EXTKEY,
 	'title'				=> 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/ExternalImport.xlf:scheduler.title',
 	'description'		=> 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/ExternalImport.xlf:scheduler.description',
 	'additionalFields'	=> \Cobweb\ExternalImport\Task\AutomatedSyncAdditionalFieldProvider::class
